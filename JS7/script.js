07019c1efdc99d8d7fc7119b94f8cb176ac4e7d4
Shabero <tokmokiuca@gmail.com>
2023-10-31 23:03:28 +0600

    new

---
+++ JS7/script.js
@@ -0,0 +1,36 @@
+// Дан массив с числами. Сделайте из него массив, состоящий из квадратов этих чисел. [1, 2, 3, 0, 4, 5, 6]
+// let num = [1, 2, 3, 0, 4, 5, 6]
+// let square = num.map((numb) => numb ** 2);
+// console.log(square);
+
+// Дан массив с числами. Проверьте то, что все элементы в массиве больше нуля. [1, 2, 3, 0, 4, 5, 6]
+// let number = [1, 2, 3, 0, 4, 5, 6]
+// let isPositive = number.every((numb) => numb > 0);
+// console.log(isPositive);
+
+// Дан массив с числами. Проверьте то, что в нем есть отрицательные элементы. [1, 2, -3, 0, 4, -5, 6]
+// let number = [1, 2, -3, 0, 4, -5, 6]
+// let isNegative = number.some((numb) => numb < 0);
+// console.log(isNegative);
+
+// Дан массив с числами. Оставьте в нем только отрицательные числа. [1, 2, -3, 0, 4, 5, -6]
+// let number = [1, 2, -3, 0, 4, 5, -6]
+// let isNegative = number.filter((numb) => numb < 0);
+// console.log(isNegative);
+
+// Дан массив с числами. Оставьте в нем только четные числа. [1, -2, 3, 0, 4, -5, 6]
+// let number = [1, -2, 3, 0, 4, -5, 6]
+// let isEven = number.filter((numb) => numb % 2 === 0);
+// console.log(isEven);
+
+// Дан массив со строками. Оставьте в нем только те строки, длина которых больше 5-ти символов.['hello', 'sit', 'ipsum', 'javascript', 'python', 'sun, 'even]
+// let str = ['hello', 'sit', 'ipsum', 'javascript', 'python', 'sun', 'even']
+// let longStr = str.filter((str) => str.length > 5);
+// console.log(longStr);
+
+// Дан массив с числами. Посчитайте количество отрицательных чисел в этом массиве.[1, 2, -3, 0, -4, 5, -6]
+// let numbers = [1, 2, -3, 0, -4, 5, -6]
+// let countNegative = numbers.filter((numb) => numb < 0).length;
+// console.log(countNegative);
+
+
